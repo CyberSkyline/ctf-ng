17f3d296d85347c867556ee5e1923c4573f517ca
Franz Payer <fpayer@cyberskyline.com>
2025-05-16 14:58:57 +0000

    Testing ctfd importing

--- backend/ctfd/plugin/__init__.py
+++ backend/ctfd/plugin/__init__.py
@@ -2,10 +2,19 @@
 
 from flask import render_template
 
+from CTFd.utils.challenges import get_all_challenges
+
+def print_challenges():
+    challenges = get_all_challenges()
+
+    print(challenges, flush=True)
+
 def load(app):
-    print("Plugin loaded")
-  
+    print_challenges()
+
     @app.route('/hello', defaults={'subpath':''}, methods=['GET'], strict_slashes=False)
     @app.route('/hello/<path:subpath>', methods=['GET'])
     def hello(subpath):
+        print_challenges()
+
         return render_template('entrypoint.html')
