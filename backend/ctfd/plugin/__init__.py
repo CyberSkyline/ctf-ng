ccf5cb9cdfef94c097cdf6c9dc3692bd009dad00
Franz Payer <fpayer@cyberskyline.com>
2025-05-16 19:50:30 +0000

    Removed test

--- backend/ctfd/plugin/__init__.py
+++ backend/ctfd/plugin/__init__.py
@@ -2,19 +2,8 @@
 
 from flask import render_template
 
-from CTFd.utils.challenges import get_all_challenges
-
-def print_challenges():
-    challenges = get_all_challenges()
-
-    print(challenges, flush=True)
-
 def load(app):
-    print_challenges()
-
     @app.route('/hello', defaults={'subpath':''}, methods=['GET'], strict_slashes=False)
     @app.route('/hello/<path:subpath>', methods=['GET'])
     def hello(subpath):
-        print_challenges()
-
         return render_template('entrypoint.html')
