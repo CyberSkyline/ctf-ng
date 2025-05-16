0083ac2a2ec3c451a8e18c5c7a382658571dbb26
Caitlin Jones <cjones@cyberskyline.com>
2025-05-16 13:11:24 +0000

    Add temp routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,18 +1,48 @@
 import { useRoutes } from "react-router";
+import Dashboard from "routes/dashboard";
+import Notifications from 'routes/notifications'
+import Profile from "routes/profile";
+import Scoreboard from "routes/scoreboard";
+import Support from 'routes/support'
 
-function TempRoute(){
-    return <div>temp route</div>
-}
+//events
+import Overview from "routes/events/Overview";
+import Registration from 'routes/events/Registration'
+import Challenge from 'routes/events/Challenge'
 
-function TempRoute2(){
-    return <div>temp route 2</div>
-}
+//Admin Section
+import AdminDashboard from "routes/admin/dashboard";
+import AdminNotifications from 'routes/admin/notifications';
+import AdminUsers from "routes/admin/users";
+import AdminTickets from "routes/admin/tickets";
+
 
 function Routes(){
     const prefix = 'hello'
     const routes = useRoutes([
-        {path: `/${prefix}`, element: <TempRoute/>},
-        {path: `/${prefix}/temp`, element: <TempRoute2/>},
+        {path: `/${prefix}`, element: <Dashboard/>},
+        {
+            path: `/${prefix}/events/:idEvent`,
+            children: [
+                {index: true, element: <Overview/>},
+                {path: `registration`, element: <Registration/>},
+                {path: `challenge/:idChallenge`, element: <Challenge/>},
+            ],
+        },
+        {path: `/${prefix}/notifications/:idNotif`, element: <Notifications/>},
+        {path: `/${prefix}/profile`, element: <Profile/>},
+        {path: `/${prefix}/scoreboard/:idEvent`, element: <Scoreboard/>},        
+        {path: `/${prefix}/support`, element: <Support/>},
+        
+        {path: `/${prefix}/admin`,
+            children: [
+                {path: `dashboard`, element: <AdminDashboard/>},
+                {path: `notifications`, element: <AdminNotifications/>},
+                {path: `users`, element: <AdminUsers/>},
+                {path: `tickets`, element: <AdminTickets/>}
+            ]
+        }
+        
     ])
     
     return (
---
+++ frontend/src/routes/admin/tickets/index.tsx
@@ -0,0 +1,3 @@
+export default function AdminTickets(){
+    return <div>admin temp route tickets</div>
+}
