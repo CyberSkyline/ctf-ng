67d6f21b4390340155183d8f9ee85ebac06b2a4d
Ubuntu <tmessner@cyberskyline.com>
2025-05-19 18:08:32 +0000

    temp

--- backend/ctfd/plugin/__init__.py
+++ backend/ctfd/plugin/__init__.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-import docker
 from .dockerutils import get_client
+import docker
 from CTFd.models import db
-
 from flask import render_template, Response
 
 class IndividualContainer(db.Model):
+    __tablename__ = 'individualcontainer'
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __init__(self, user):
         self.user = user
-        self.client = docker.from_env()
+        self.client = get_client('10.100')
 
     def __del__(self):
         self.client.close()
@@ -35,6 +35,33 @@ class IndividualContainer(db.Model):
     def disconnect_network(self, target_network):
         network = self.client.networks.get(target_network)
         network.disconnect(self.user)
+
+
+class ChallengeContainer(db.Model):
+    __tablename__ = 'challengecontainer'
+    id = db.Column(db.Integer, primary_key=True)
+    challenge = db.Column(db.Integer, db.ForeignKey('challenges.id'))
+    ## Image name
+    image = db.Column(db.String(255))
+    ## Network name
+    network = db.Column(db.String(255))
+    ## Alias/network name
+    alias = db.Column(db.String(255))
+    ## Ram override
+    ramOverride = db.Column(db.Integer)
+
+    def __init__(self, challenge):
+        self.challenge = challenge
+
+
+
+class ChallengeContianerInstance(db.Model):
+    __tablename__ = 'challengecontainerinstance'
+    id = db.Column(db.Integer, primary_key=True)
+    ## Challenge Container
+    challengeContainer = db.Column(db.Integer, db.ForeignKey('challengecontainer.id'))
+    ## Team id
+    team = db.Column(db.Integer, db.ForeignKey('teams.id'))
 
 def load(app):
     app.db.create_all()
