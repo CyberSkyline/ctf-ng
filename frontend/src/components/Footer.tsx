71b7f4aca3d18f39230f020f315d3b2134a1373c
Caitlin Jones <cjones@cyberskyline.com>
2025-06-02 15:45:17 +0000

    lint

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,3 @@
-
-
 export default function FooterBar() {
   return (
     <div>footer</div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import { BrowserRouter } from 'react-router';
+import { Theme } from '@radix-ui/themes';
 import NavBar from 'components/NavBar';
 import FooterBar from 'components/Footer';
 import Routes from './Routes';
 
 import './index.css';
-import { Theme } from '@radix-ui/themes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <Theme>
-      <NavBar />
-      <Routes />
-      <FooterBar />
-    </Theme>
+      <Theme>
+        <NavBar />
+        <Routes />
+        <FooterBar />
+      </Theme>
     </BrowserRouter>
   </StrictMode>,
 );
