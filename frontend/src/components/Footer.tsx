bc96d2401d31108892eede6eb4e7b7e1e5365707
Caitlin Jones <cjones@cyberskyline.com>
2025-06-02 12:11:26 +0000

    Remove flowbite-react

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,7 @@
-import {
-  Footer, FooterCopyright, FooterLink, FooterLinkGroup,
-} from 'flowbite-react';
+
 
 export default function FooterBar() {
   return (
-    <Footer container className="fixed bottom-0">
-      <FooterCopyright href="https://cyberskyline.com/" by="Cyber Skyline™" year={2025} />
-      <FooterLinkGroup>
-        <FooterLink href="/">Home</FooterLink>
-      </FooterLinkGroup>
-    </Footer>
+    <div>footer</div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,13 +7,16 @@ import FooterBar from 'components/Footer';
 import Routes from './Routes';
 
 import './index.css';
+import { Theme } from '@radix-ui/themes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Theme>
       <NavBar />
       <Routes />
       <FooterBar />
+    </Theme>
     </BrowserRouter>
   </StrictMode>,
 );
