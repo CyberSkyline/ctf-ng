a2d0ce07aa27dba4f41da75aeab2f4f1f659d8a7
trevnels-cs <tnelson@cyberskyline.com>
2025-06-02 15:57:43 -0400

    Use radix for event page

--- frontend/src/routes/events/Overview.tsx
+++ frontend/src/routes/events/Overview.tsx
@@ -1,55 +1,77 @@
-import Container from 'components/layout/Container';
-import { TabItem, Tabs } from 'flowbite-react';
-import { useMemo } from 'react';
+import {
+  Container, Flex, Grid, Heading, Skeleton, Tabs, Text,
+} from '@radix-ui/themes';
 import { FaRankingStar, FaTrophy, FaUserGroup } from 'react-icons/fa6';
-import { useParams, useSearchParams } from 'react-router';
+import { useSearchParams } from 'react-router';
 
 export default function Overview() {
-  const { idEvent } = useParams();
   const [searchParams, setSearchParams] = useSearchParams();
-
-  // eventually this will need logic to adjust the tab list dynamically based on event properties
-  // i.e. if the event is individual, the team tab shouldn't be present
-  const tabs = useMemo(() => [
-    {
-      key: 'challenges',
-      title: 'Challenges',
-      icon: FaTrophy,
-      content: <span>Placeholder Challenges</span>,
-    },
-    {
-      key: 'leaderboard',
-      title: 'Leaderboard',
-      icon: FaRankingStar,
-      content: <span>Placeholder Leaderboard</span>,
-    },
-    {
-      key: 'team',
-      title: 'Team',
-      icon: FaUserGroup,
-      content: <span>Placeholder Team</span>,
-    },
-  ], []);
+  const currentTab = searchParams.get('tab') ?? 'challenges';
 
   return (
-    <Container>
-      <h1 className="text-3xl text-white font-bold">{idEvent}</h1>
-      <p>Some filler text, start time, stop time, etc.</p>
+    <>
+      <Container size="1" my="4">
+        <Heading>Event Name</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+      </Container>
 
-      <Tabs variant="fullWidth" className="mt-4" onActiveTabChange={(index) => setSearchParams({ tab: tabs[index].key })}>
-        {tabs.map((tab) => (
-          <TabItem
-            key={tab.key}
-            // TODO: figure out how to set aria-hidden on icon components passed into flowbite.
-            // HoC seems to be the only way to do it, but it's janky and violates many eslint rules
-            icon={tab.icon}
-            title={tab.title}
-            active={searchParams.get('tab') === tab.key}
-          >
-            {tab.content}
-          </TabItem>
-        ))}
-      </Tabs>
-    </Container>
+      <Tabs.Root
+        value={currentTab}
+        onValueChange={(tab) => {
+          setSearchParams((prev) => {
+            prev.set('tab', tab);
+            return prev;
+          });
+        }}
+      >
+        <Container size="2">
+          <Tabs.List className="*:!basis-0 *:!grow" loop={false}>
+            <Tabs.Trigger value="challenges">
+              <FaTrophy className="mr-1" />
+              Challenges
+            </Tabs.Trigger>
+            <Tabs.Trigger value="leaderboard">
+              <FaRankingStar className="mr-1" />
+              Leaderboard
+            </Tabs.Trigger>
+            <Tabs.Trigger value="team">
+              <FaUserGroup className="mr-1" />
+              Team
+            </Tabs.Trigger>
+          </Tabs.List>
+        </Container>
+
+        <Tabs.Content value="challenges">
+          <Container size="4">
+            <Grid columns={{ xs: '1', sm: '2', md: '4' }} gap="4" p="4">
+              <Skeleton height="128px" />
+              <Skeleton height="128px" />
+              <Skeleton height="128px" />
+              <Skeleton height="128px" />
+              <Skeleton height="128px" />
+            </Grid>
+          </Container>
+        </Tabs.Content>
+
+        <Tabs.Content value="leaderboard">
+          <Container size="4">
+            <Flex direction="column" gap="2" p="2">
+              <Skeleton height="64px" />
+              <Skeleton height="64px" />
+              <Skeleton height="64px" />
+              <Skeleton height="64px" />
+              <Skeleton height="64px" />
+            </Flex>
+          </Container>
+        </Tabs.Content>
+
+        <Tabs.Content value="team">
+          <Text>Placeholder team info</Text>
+        </Tabs.Content>
+      </Tabs.Root>
+    </>
   );
 }
