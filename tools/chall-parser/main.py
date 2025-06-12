6ff235d1fc2b35786f1b5f280a37122c7533791a
John Marsden <research@johnmarsden.dev>
2025-06-11 21:06:49 -0400

    Add challenge parser to new tools directory

---
+++ tools/chall-parser/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chall-parser!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/chall-parser/parser/rewriter.py
@@ -0,0 +1,81 @@
+import logging
+from typing import Generator
+import typing
+from attrs import define
+from yaml import BaseLoader, Event, MappingStartEvent, ScalarEvent
+import yaml
+logger = logging.getLogger(__name__)
+
+
+def rewrite_variable(loader: BaseLoader):
+    logger.debug("Entering rewrite_variable")
+    yield loader.get_event()
+    events = {}
+    while loader.check_event(ScalarEvent):
+        key_event = loader.get_event()
+        logger.debug(f"Key event value: {key_event.value}")
+        events[key_event.value] = (key_event, loader.get_event())
+        logger.debug(f"Events dict updated: {events}")
+    
+    if 'template' not in events:
+        logger.debug("No 'template' key found in variable")
+        return
+    template_key, template_value = events['template']
+    logger.debug(f"Template key: {template_key}, value: {template_value}")
+    if 'default' not in events:
+        logger.debug("No 'default' key found in variable")
+        return
+    default_key, default_value = events['default']
+    logger.debug(f"Default key: {default_key}, value: {default_value}")
+    template_value.anchor = default_value.anchor
+    template_value.tag = '!template'
+    default_value.anchor = None
+    logger.debug(f"Setting template value anchor to: {template_value.anchor}")
+    yield from (template_key, template_value, default_key, default_value)
+    yield loader.get_event()
+
+def rewrite_variables(loader):
+    logger.debug("Entering rewrite_variables")
+    if not loader.check_event(ScalarEvent):
+        logger.debug("No ScalarEvent at start of rewrite_variables")
+        return
+    event = loader.get_event()
+    logger.debug(f"Got event: {event}")
+    yield event
+    if event.value != "variables":
+        logger.debug(f"Event value is not 'variables': {event.value}")
+        return
+    if not loader.check_event(MappingStartEvent):
+        logger.debug("No MappingStartEvent after 'variables'")
+        return
+    
+    yield loader.get_event()
+    logger.debug("Processing variables")
+    while loader.check_event(ScalarEvent):
+        yield loader.get_event()
+        if not loader.check_event(MappingStartEvent):
+            logger.debug("No MappingStartEvent after variable key")
+            return
+        yield from rewrite_variable(loader)
+    return
+
+
+def rewrite_aliases(loader: BaseLoader) -> Generator[Event]:
+    logger.debug("Entering rewrite_aliases")
+    while True:
+        if loader.check_event(MappingStartEvent):
+            logger.debug("Found MappingStartEvent in rewrite_aliases")
+            yield loader.get_event()
+            yield from rewrite_variables(loader)
+        elif loader.check_event():
+            logger.debug("Found other event in rewrite_aliases")
+            yield loader.get_event()
+        else:
+            logger.debug("No more events in rewrite_aliases")
+            break
+
+@define
+class Template(yaml.YAMLObject):
+    template: str
+    
+    yaml_tag = '!template'
