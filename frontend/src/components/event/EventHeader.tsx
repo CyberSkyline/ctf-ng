407d8a7b6a97b97795cf19e56dba3ec2b4640dc5
trevnels-cs <tnelson@cyberskyline.com>
2025-06-13 14:47:31 -0400

    Dashboard scaffolding & updates to event page for consistency

--- frontend/src/components/event/EventHeader.tsx
+++ frontend/src/components/event/EventHeader.tsx
@@ -1,36 +1,37 @@
 import {
-  Heading, Text, Button,
-  Flex,
-  Box,
-  AspectRatio,
+  Heading, Text, Flex, Box, AspectRatio,
 } from '@radix-ui/themes';
-import HeaderContainer from 'components/HeaderContainer';
+import EventBadge from './EventBadge';
 
 export default function EventHeader({
   name,
   description,
+  state,
+  children = undefined,
 }: {
     name: string;
     description: string;
+    state: 'upcoming' | 'waiting' | 'live' | 'ended';
+    children?: React.ReactNode;
 }) {
   return (
-    <HeaderContainer>
-      <Flex direction="row" gap="6" align="start">
-        <Box className="w-32">
-          <AspectRatio ratio={10 / 16}>
-            {/* Placeholder for event card image */}
-            <Box className="h-full w-full bg-[var(--lime-8)] rounded-xl shadow-xl" />
-          </AspectRatio>
+    <Flex direction="row" gap="6" align="start">
+      <Box className="w-32" flexShrink="0">
+        <AspectRatio ratio={3 / 4}>
+          {/* Placeholder for event card image */}
+          <Box className="h-full w-full bg-[var(--lime-8)] rounded-lg shadow-xl" />
+        </AspectRatio>
+      </Box>
+      <Flex direction="column" flexGrow="1" align="start" gap="2">
+        {state && (
+          <EventBadge state={state} />
+        )}
+        <Box>
+          <Heading size="9">{name}</Heading>
+          <Text as="p" color="gray">{description}</Text>
         </Box>
-        <Box flexGrow="1">
-          <Heading size="9" color="lime">{name}</Heading>
-          <Text as="p">{description}</Text>
-          <Flex direction="row" gap="2">
-            <Button variant="solid" color="lime" size="2" mt="4">Button 1</Button>
-            <Button variant="soft" color="lime" size="2" mt="4">Button 2</Button>
-          </Flex>
-        </Box>
+        {children}
       </Flex>
-    </HeaderContainer>
+    </Flex>
   );
 }
--- frontend/src/routes/dashboard/index.tsx
+++ frontend/src/routes/dashboard/index.tsx
@@ -1,3 +1,57 @@
+import { Button, Container, Flex } from '@radix-ui/themes';
+import PastEvents from 'components/dashboard/PastEvents';
+import UpcomingEvents from 'components/dashboard/UpcomingEvents';
+import EventHeader from 'components/event/EventHeader';
+import HeaderContainer from 'components/HeaderContainer';
+
 export default function Dashboard() {
-  return <div>temp route</div>;
+  return (
+    <>
+      <HeaderContainer>
+        { /* Events that are currently active or starting soon emphasized in the page header. */ }
+        <EventHeader name="PC7 Teams Round 1" description="A brief description of what this event is." state="live">
+          <Button variant="solid" size="2">Button 1</Button>
+        </EventHeader>
+      </HeaderContainer>
+
+      <Container size="4">
+        <Flex direction="column" gap="4" my="8">
+          <UpcomingEvents events={[
+            {
+              id: 'pc7-track-a', name: 'PC7 Track A', color: 'red', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc7-track-b', name: 'PC7 Track B', color: 'jade', description: 'A brief description of what this event is.',
+            },
+          ]}
+          />
+
+          <PastEvents events={[
+            {
+              id: 'pc6-team', name: 'PC6 Teams Round 1', color: 'lime', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc5-track-a', name: 'PC5 Track A', color: 'red', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc5-track-b', name: 'PC5 Track B', color: 'jade', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc4-teams-finals', name: 'PC4 Teams Finals', color: 'orange', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc4-track-a', name: 'PC4 Track A', color: 'plum', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc4-track-b', name: 'PC4 Track B', color: 'blue', description: 'A brief description of what this event is.',
+            },
+            {
+              id: 'pc3-teams', name: 'PC3 Teams', color: 'yellow', description: 'A brief description of what this event is.',
+            },
+          ]}
+          />
+        </Flex>
+      </Container>
+    </>
+  );
 }
