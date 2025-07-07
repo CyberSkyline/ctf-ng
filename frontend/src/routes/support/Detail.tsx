81464e07000b4ff3879d40cc5bbf402b3bfc2656
Caitlin Jones <cjones@cyberskyline.com>
2025-07-07 19:56:35 +0000

    Remove extra space from icon button

--- frontend/src/routes/support/Detail.tsx
+++ frontend/src/routes/support/Detail.tsx
@@ -42,7 +42,6 @@ export default function Detail() {
             onClick={() => { navigate('/support'); }}
           >
             <TbArrowLeft />
-            {' '}
             Support
           </Button>
         </Box>
