cc0e401134eeb5f645eb1cd9495b24c17afc562e
trevnels-cs <tnelson@cyberskyline.com>
2025-07-10 15:49:27 -0400

    Update SWR based on backend changes and auto-parse dates

--- frontend/src/hooks/team.ts
+++ frontend/src/hooks/team.ts
@@ -1,38 +1,54 @@
 import type { Team, TeamMember } from '@/types';
 import useSWR from 'swr';
 
-export function useTeams() {
-  return useSWR<{
-    teams: Team[],
-    total_teams: number
-  }, Error>(
-    '/teams/all',
+/**
+ * Get a list of all teams across the system.
+ * This is an admin-only endpoint.
+ */
+export function useAllTeams() {
+  return useSWR<Team[], Error>(
+    '/admin/teams',
   );
 }
 
-export function useEventTeams(eventId: number | null) {
-  return useSWR<{
-    teams: Team[],
-    total_teams: number,
-    event_name: string
-  }, Error>(
-    eventId ? `/events/${eventId}/teams` : null,
-  );
-}
-
+/**
+ * Get a list of teams the given user is part of.
+ * This is an admin-only endpoint.
+ * @param userId The ID of the user to fetch teams for, or null if this should not be fetched.
+ */
 export function useUserTeams(userId: number | null) {
-  return useSWR<{
-    teams: Team[]
-  }, Error>(
-    userId ? `/users/${userId}/teams` : null,
+  return useSWR<Team[], Error>(
+    userId ? `/admin/users/${userId}/teams` : null,
   );
 }
 
+/**
+ * Get a list of teams the currently signed in user is part of.
+ */
+export function useMyTeams() {
+  return useSWR<Team[], Error>(
+    '/users/me/teams',
+  );
+}
+
+/**
+ * Get information about a specific team by ID.
+ * This is an admin-only endpoint.
+ * @param teamId The ID of the team to fetch, or null if this should not be fetched.
+ */
 export function useTeam(teamId: number | null) {
-  return useSWR<{
-    team: Team,
-    team_members: TeamMember[]
-  }, Error>(
-    teamId ? `/teams/${teamId}` : null,
+  return useSWR<Team, Error>(
+    teamId ? `/admin/teams/${teamId}` : null,
+  );
+}
+
+/**
+ * Get a list of members for a specific team.
+ * This is an admin-only endpoint.
+ * @param teamId The ID of the team to fetch members for, or null if this should not be fetched.
+ */
+export function useTeamMembers(teamId: number | null) {
+  return useSWR<TeamMember[], Error>(
+    teamId ? `/admin/teams/${teamId}/members` : null,
   );
 }
