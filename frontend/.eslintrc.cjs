cc830e56eae049401229f4de3a709a8132b3df89
trevnels-cs <tnelson@cyberskyline.com>
2025-07-15 14:43:25 -0400

    Configure eslint import-newlines

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
     'plugin:react-hooks/recommended', // Recommended React Hooks rules
     'plugin:jsx-a11y/recommended', // Accessibility rules for JSX
   ],
-  plugins : [ '@typescript-eslint', 'react', 'react-hooks' ], // Enable TypeScript, React, and React Hooks plugins
+  plugins : [ '@typescript-eslint', 'react', 'react-hooks', 'import-newlines' ], // Enable TypeScript, React, and React Hooks plugins
   rules : {
     // Customize any rules here
     'key-spacing' : [ 'error', {
@@ -42,6 +42,11 @@ module.exports = {
     'react/jsx-filename-extension' : [ 'error', { extensions : [ '.js', '.jsx', '.ts', '.tsx' ] } ],
     'import/no-extraneous-dependencies' : [ 'error', { devDependencies : true } ],
     'react/require-default-props' : 'off',
+    'import-newlines/enforce' : [ 'error', {
+      items : 3, // Number of imports before enforcing new lines. Must be kept in sync with airbnb's object-curly-newline rule.
+      'max-len' : 160, // Maximum line length for imports. Must be kept in sync with max-len rule above.
+      semi : true, // Imports should end with a semicolon
+    } ],
   },
   settings : {
     react : {
