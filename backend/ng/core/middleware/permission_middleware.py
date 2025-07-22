95cbdf5e8c7acde4a9d7968ac04183ef6ec060ff
Daniel Rogers <drogers@cyberskyline.com>
2025-07-21 22:57:05 -0700

    Change not event check

--- backend/ng/core/middleware/permission_middleware.py
+++ backend/ng/core/middleware/permission_middleware.py
@@ -38,7 +38,7 @@ def event_only_public(f):
         event = kwargs.get('event')
         current_user = kwargs.get('current_user')
         if Demographic.find_by_user_and_event(current_user.id, event.id) is None:
-            if not event or not event.public:
+            if not event.public:
                 return error_response("Event not found", "not_found", 404)
         return f(*args, **kwargs)
     return wrapped
