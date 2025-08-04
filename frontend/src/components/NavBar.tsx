9af17bc9e6430cda0d82182aecd1199b13fa80cd
Caitlin Jones <cjones@cyberskyline.com>
2025-08-04 18:04:16 +0000

    update navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,59 +1,78 @@
 import { NavigationMenu } from 'radix-ui';
 import { DropdownMenu } from '@radix-ui/themes';
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
+import { TbUserCircle } from 'react-icons/tb';
+import { twMerge } from 'tailwind-merge';
 
 export default function NavBar() {
-  function logout() {
+  const logout = () => {
     // perform a logout
-  }
+  };
+
+  const location = useLocation();
+
+  const defaultLinkClass = `
+    p-2
+    hover:bg-(--gray-3)
+    dark:hover:bg-(--gray-3)
+    dark:hover:text-(--gray-12)
+    text-(--gray-a11)
+    dark:text-(--gray-a11)`;
+
+  const activeLinkClass = `
+    p-2
+    hover:bg-(--gray-3)
+    dark:hover:bg-(--gray-3)
+    text-(--gray-a11)
+    dark:text-(--gray-12)
+    underline
+    decoration-(--accent-indicator)
+    underline-offset-8
+    decoration-2`;
 
   return (
     <NavigationMenu.Root className="h-[var(--NavBarHeight)]">
-      <NavigationMenu.List className="flex p-1 shadow-md pr-4">
+      <NavigationMenu.List className="flex p-1 pr-4 dark:border-b-(--gray-a6) dark:border-b-1">
         <div className="flex">
-          <NavigationMenu.Item className="p-2">
+          <NavigationMenu.Item className={location.pathname === '/' ? activeLinkClass : defaultLinkClass}>
             <NavLink to="/">
               Dashboard
             </NavLink>
           </NavigationMenu.Item>
-          <NavigationMenu.Item className="p-2">
+          <NavigationMenu.Item className={location.pathname === '/events' ? activeLinkClass : defaultLinkClass}>
             <NavLink to="/events">
-              Events*
+              Events
             </NavLink>
           </NavigationMenu.Item>
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/">
-              Practice*
-            </NavLink>
+          <NavigationMenu.Item className={location.pathname === '/practice' ? activeLinkClass : defaultLinkClass}>
+            Practice*
           </NavigationMenu.Item>
         </div>
         <div className="flex ml-auto">
-          <NavigationMenu.Item className="p-2">
+          <NavigationMenu.Item className={location.pathname === '/support' ? activeLinkClass : defaultLinkClass}>
             <NavLink to="/support">
               Support
             </NavLink>
           </NavigationMenu.Item>
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/">
-              Notifications*
-            </NavLink>
+          <NavigationMenu.Item className={location.pathname === '/notifications' ? activeLinkClass : defaultLinkClass}>
+            Notifications*
           </NavigationMenu.Item>
 
           <DropdownMenu.Root>
             <DropdownMenu.Trigger>
-              <button type="button" className="p-2">
-                Profile
-              </button>
+              <NavigationMenu.Item className={twMerge(defaultLinkClass, 'pt-3')}>
+                <TbUserCircle />
+              </NavigationMenu.Item>
             </DropdownMenu.Trigger>
             <DropdownMenu.Content
               className="mt-2 rounded-md shadow-lg p-2 border"
             >
               <DropdownMenu.Item asChild>
                 <NavLink
-                  to="/profile"
+                  // to="/profile"
                   className="flex items-center gap-2 px-2 py-1.5 rounded"
                 >
-                  Profile
+                  Profile*
                 </NavLink>
               </DropdownMenu.Item>
 
@@ -69,15 +88,16 @@ export default function NavBar() {
               <DropdownMenu.Item asChild>
                 <button
                   type="button"
-                  onClick={() => logout()}
+                  onClick={logout}
                   className="w-full flex items-center gap-2 px-2 py-1.5 rounded"
                 >
-                  Log Out
+                  Log Out*
                 </button>
               </DropdownMenu.Item>
             </DropdownMenu.Content>
           </DropdownMenu.Root>
         </div>
+        <NavigationMenu.Indicator className="NavigationMenuIndicator" />
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
