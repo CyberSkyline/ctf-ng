bed81c149af8274b6a8b8a0304f39feea288405f
cjones-cs <cjones@cyberskyline.com>
2025-08-04 14:35:43 -0400

    update navbar (#97)
    * update navbar
    
    * fixes

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,66 +1,88 @@
 import { NavigationMenu } from 'radix-ui';
 import { DropdownMenu } from '@radix-ui/themes';
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
+import { TbUserCircle } from 'react-icons/tb';
+import { twMerge } from 'tailwind-merge';
 
 export default function NavBar() {
-  function logout() {
+  const logout = () => {
     // perform a logout
-  }
+  };
+
+  const location = useLocation();
+
+  const defaultLinkClass = `
+    p-2
+    hover:bg-(--gray-3)
+    dark:hover:bg-(--gray-3)
+    dark:hover:text-(--gray-12)
+    text-(--gray-a11)
+    dark:text-(--gray-a11)`;
+
+  const activeLinkClass = `
+    p-2
+    hover:bg-(--gray-3)
+    dark:hover:bg-(--gray-3)
+    text-(--gray-a11)
+    dark:text-(--gray-12)
+    underline
+    decoration-(--accent-indicator)
+    underline-offset-8
+    decoration-2`;
+
+  const dropdownClass = 'flex items-center gap-2 px-2 py-1.5 rounded';
 
   return (
     <NavigationMenu.Root className="h-[var(--NavBarHeight)]">
-      <NavigationMenu.List className="flex p-1 shadow-md pr-4">
+      <NavigationMenu.List className="flex p-1 pr-4 dark:border-b-(--gray-a6) dark:border-b-1">
         <div className="flex">
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/">
+          <NavLink to="/" className={location.pathname === '/' ? activeLinkClass : defaultLinkClass}>
+            <NavigationMenu.Item>
               Dashboard
-            </NavLink>
-          </NavigationMenu.Item>
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/events">
-              Events*
-            </NavLink>
-          </NavigationMenu.Item>
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/">
+            </NavigationMenu.Item>
+          </NavLink>
+          <NavLink to="/events" className={location.pathname === '/events' ? activeLinkClass : defaultLinkClass}>
+            <NavigationMenu.Item>
+              Events
+            </NavigationMenu.Item>
+          </NavLink>
+          <NavLink className={location.pathname === '/practice' ? activeLinkClass : defaultLinkClass}>
+            <NavigationMenu.Item>
               Practice*
-            </NavLink>
-          </NavigationMenu.Item>
+            </NavigationMenu.Item>
+          </NavLink>
         </div>
         <div className="flex ml-auto">
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/support">
+          <NavLink to="/support" className={location.pathname === '/support' ? activeLinkClass : defaultLinkClass}>
+            <NavigationMenu.Item>
               Support
-            </NavLink>
-          </NavigationMenu.Item>
-          <NavigationMenu.Item className="p-2">
-            <NavLink to="/">
-              Notifications*
-            </NavLink>
+            </NavigationMenu.Item>
+          </NavLink>
+          <NavigationMenu.Item className={location.pathname === '/notifications' ? activeLinkClass : defaultLinkClass}>
+            Notifications*
           </NavigationMenu.Item>
 
           <DropdownMenu.Root>
             <DropdownMenu.Trigger>
-              <button type="button" className="p-2">
-                Profile
-              </button>
+              <NavigationMenu.Item className={twMerge(defaultLinkClass, 'pt-3')}>
+                <TbUserCircle />
+              </NavigationMenu.Item>
             </DropdownMenu.Trigger>
             <DropdownMenu.Content
               className="mt-2 rounded-md shadow-lg p-2 border"
             >
               <DropdownMenu.Item asChild>
                 <NavLink
-                  to="/profile"
-                  className="flex items-center gap-2 px-2 py-1.5 rounded"
+                  // to="/profile"
+                  className={dropdownClass}
                 >
-                  Profile
+                  Profile*
                 </NavLink>
               </DropdownMenu.Item>
-
               <DropdownMenu.Item asChild>
                 <NavLink
                   to="/admin"
-                  className="flex items-center gap-2 px-2 py-1.5 rounded"
+                  className={dropdownClass}
                 >
                   Admin Portal
                 </NavLink>
@@ -69,15 +91,16 @@ export default function NavBar() {
               <DropdownMenu.Item asChild>
                 <button
                   type="button"
-                  onClick={() => logout()}
-                  className="w-full flex items-center gap-2 px-2 py-1.5 rounded"
+                  onClick={logout}
+                  className={dropdownClass}
                 >
-                  Log Out
+                  Log Out*
                 </button>
               </DropdownMenu.Item>
             </DropdownMenu.Content>
           </DropdownMenu.Root>
         </div>
+        <NavigationMenu.Indicator className="NavigationMenuIndicator" />
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
