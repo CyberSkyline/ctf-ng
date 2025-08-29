d7b66e15a89895aa5c0d9d430de0b32a34828044
trevnels-cs <tnelson@cyberskyline.com>
2025-08-29 13:55:52 -0400

    Dashboard refactor

--- frontend/src/routes/dashboard/PastEvents.tsx
+++ frontend/src/routes/dashboard/PastEvents.tsx
@@ -1,9 +1,23 @@
-import type { Event } from '@/types';
+import { useMyEvents } from '@/hooks/events';
 import { Grid, Heading } from '@radix-ui/themes';
+import { ErrorCallout } from 'components/Callouts';
 import EventCard from './EventCard';
 
-export default function PastEvents({ events }: { events: Event[] }) {
-  if (events.length === 0) {
+export default function PastEvents() {
+  const { data, error } = useMyEvents();
+  const pastEvents = data?.filter((event) => event.end_time && new Date() > event.end_time);
+
+  if (error) {
+    return (
+      <>
+        <Heading size="6">Your Past Events</Heading>
+        <ErrorCallout>{error.message}</ErrorCallout>
+      </>
+    );
+  }
+
+  if (pastEvents === undefined || pastEvents.length === 0) {
+    // Show nothing if there are no past events or if data is still loading
     return null;
   }
 
@@ -16,7 +30,7 @@ export default function PastEvents({ events }: { events: Event[] }) {
         }}
         gap="4"
       >
-        {events.map((event) => (
+        {pastEvents.map((event) => (
           <EventCard
             key={event.id}
             event={event}
--- frontend/src/routes/dashboard/index.tsx
+++ frontend/src/routes/dashboard/index.tsx
@@ -1,21 +1,22 @@
 import { useMyEvents } from '@/hooks/events';
-import { Button, Container, Flex } from '@radix-ui/themes';
+import {
+  Container,
+  Flex,
+  Heading,
+  Skeleton,
+} from '@radix-ui/themes';
 import { ErrorCallout, InfoCallout } from 'components/Callouts';
 import EventHeader from 'components/EventHeader';
 import HeaderContainer from 'components/HeaderContainer';
-import { Link } from 'react-router';
 import PastEvents from 'routes/dashboard/PastEvents';
 import UpcomingEvents from 'routes/dashboard/UpcomingEvents';
 
 export default function Dashboard() {
-  const { data, error } = useMyEvents();
+  const { data, error, isLoading } = useMyEvents();
 
-  // Split registered events into past, present, and future
-  const upcomingEvents = data?.filter((event) => !event.start_time || new Date() < event.start_time) || [];
-  const pastEvents = data?.filter((event) => event.end_time && new Date() > event.end_time) || [];
   const liveEvents = data?.filter(
     (event) => event.start_time && event.end_time && new Date() >= event.start_time && new Date() <= event.end_time,
-  ) || [];
+  );
 
   if (error) {
     return <ErrorCallout>{error.message}</ErrorCallout>;
@@ -24,30 +25,27 @@ export default function Dashboard() {
   return (
     <>
       <HeaderContainer>
-        {liveEvents.length === 0 && (
-          <InfoCallout>
-            No events are currently running. This should eventually be something more interesting, i.e. the first upcoming event or practice area.
-          </InfoCallout>
+        {(liveEvents === undefined || liveEvents.length === 0) && (
+          <Skeleton loading={isLoading}>
+            <InfoCallout>
+              No events are currently running. This should eventually be something more interesting, i.e. the first upcoming event or practice area.
+            </InfoCallout>
+          </Skeleton>
         )}
-        {liveEvents.map((event) => (
+        {liveEvents?.map((event) => (
           <EventHeader
             key={event.id}
             event={event}
-          >
-            <Button asChild>
-              <Link to={`/events/${event.id}`}>
-                Go
-              </Link>
-            </Button>
-          </EventHeader>
+          />
         ))}
       </HeaderContainer>
 
       <Container size="4">
         <Flex direction="column" gap="4" my="8">
-          <UpcomingEvents events={upcomingEvents} />
+          <Heading size="6">Your Upcoming Events</Heading>
+          <UpcomingEvents />
 
-          <PastEvents events={pastEvents} />
+          <PastEvents />
         </Flex>
       </Container>
     </>
