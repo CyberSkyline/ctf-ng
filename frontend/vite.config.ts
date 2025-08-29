795906dd0155fd2e83a0937494a25f14fa47631b
Zoe Coughlan <zcoughlan@cyberskyline.com>
2025-08-29 18:09:13 +0000

    allowing vite on vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   server : {
-    allowedHosts : [ '.cisa.gov', '.localhost' ],
+    allowedHosts : [ '.cisa.gov', '.localhost', 'vite' ],
     host : '0.0.0.0',
     cors : true,
     hmr : {
