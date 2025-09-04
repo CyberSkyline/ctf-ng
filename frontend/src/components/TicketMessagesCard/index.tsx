336fa6ff735d0ecb0f5e7362adf8d8b1fd7f6bf2
Caitlin Jones <cjones@cyberskyline.com>
2025-09-04 19:18:26 +0000

    Fix dates displayed

--- frontend/src/components/TicketMessagesCard/index.tsx
+++ frontend/src/components/TicketMessagesCard/index.tsx
@@ -33,7 +33,7 @@ export default function TicketMessagesCard({
         >
           <Flex justify="between">
             <Text weight="bold" size="2">{message.author_name}</Text>
-            <Text weight="bold" size="2">{new Date(message.created_at).toString()}</Text>
+            <Text weight="bold" size="2">{message.created_at.toLocaleString()}</Text>
           </Flex>
           <Separator size="4" className="mb-1" />
           <Text as="p">
