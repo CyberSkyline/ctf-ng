7112ae4cd9300f1617702b6908a71879be10a974
Franz Payer <fpayer@cyberskyline.com>
2025-09-23 16:00:06 +0000

    Fix bad copy+paste

--- .bin/default_ctfd_setup.sh
+++ .bin/default_ctfd_setup.sh
@@ -7,7 +7,7 @@ source $DIR/utils.sh
 
 check_ctfd_running
 
-highlight "THIS WILL DELETE THE EXISTING DATABASE AND INSERT SAMPLE DATA\n"
+highlight "THIS WILL DELETE THE EXISTING DATABASE AND RESET IT TO THE DEFAULT CTFD SETUP\n"
 
 prompt_user "Are you sure you want to continue?" && {
   docker exec ng-ctfd /bin/bash -c "PYTHONPATH=/opt/CTFd/ SCRIPT=true /opt/CTFd/CTFd/plugins/ng/scripts/default_ctfd_setup.py"
