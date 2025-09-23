ce9741d388d93d0ac64dcb17d6339076f654943a
trevnels-cs <tnelson@cyberskyline.com>
2025-09-23 15:40:27 -0400

    Refetch correct endpoints for other event types

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -11,4 +11,15 @@ socket.on('refetch', (msg: {path: string}) => {
   mutate(resourcePath);
 });
 
+// Update notification hooks when a notification is received.
+socket.on('notification', () => {
+  mutate('/notifications/me');
+  mutate('/notifications/unread_count');
+});
+
+// Update announcements hook when an announcement is received.
+socket.on('system_announcement', () => {
+  mutate('/notifications/announcements');
+});
+
 export default socket;
