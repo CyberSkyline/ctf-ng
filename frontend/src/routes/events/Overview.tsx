5b2908344ad03634628b2e918823bbab88043bfc
cjones-cs <cjones@cyberskyline.com>
2025-09-25 17:25:45 -0400

    Fix variable passing (#194)

--- frontend/src/routes/events/Overview.tsx
+++ frontend/src/routes/events/Overview.tsx
@@ -26,7 +26,7 @@ export default function Overview() {
           <EventHeader
             event={data}
           >
-            {data && <StartModal eventId={data.id} />}
+            {data && <StartModal event={data} />}
           </EventHeader>
         )}
       </HeaderContainer>
