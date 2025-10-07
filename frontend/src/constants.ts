aa503a3810c4988b7804098ac178e1f5fdd55f39
Caitlin Jones <cjones@cyberskyline.com>
2025-10-07 18:46:14 +0000

    Add constants and type

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -65,3 +65,17 @@ export const ROLES: Record<string, { color: AccentColor; icon: IconType }> = {
     icon : UserIcon,
   },
 };
+
+
+/* Find these const defined in NotificationType.py */
+export const NOTIF_TYPE = {
+  TICKETS : [
+    "ticket_create",
+    "ticket_message",
+    "ticket_status_change",
+    "ticket_assigned",
+  ],
+  TEAM: [
+    "team_invitation",
+  ]
+};
