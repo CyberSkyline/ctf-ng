19f2cee81e382d3a0918dcacce63123f7638d687
Franz Payer <fpayer@cyberskyline.com>
2025-10-07 10:48:54 -0400

    Adds #232 (#233)
    Add userId to window.init

--- backend/ng/core/routes/views.py
+++ backend/ng/core/routes/views.py
@@ -3,12 +3,14 @@ All user-accessed URLs use the same view for the frontend application.
 """
 
 from typing import Any
-from flask import Blueprint, render_template
-from flask import current_app as app, session, request
-from sqlalchemy.exc import IntegrityError
-from CTFd.utils import get_app_config
+
 from CTFd.models import Admins, db
+from CTFd.utils import get_app_config
 from CTFd.utils.security.csrf import generate_nonce
+from CTFd.utils.user import get_current_user
+from flask import Blueprint, render_template, request, session
+from flask import current_app as app
+from sqlalchemy.exc import IntegrityError
 
 plugin_views = Blueprint("plugin_views", __name__)
 
@@ -17,10 +19,13 @@ plugin_views = Blueprint("plugin_views", __name__)
 @plugin_views.route("/<path:subpath>", methods=["GET"])
 def view_template(subpath: str) -> Any:
     static_build_path = get_app_config("STATIC_BUILD_PATH")
+    user = get_current_user()
     return render_template(
         "dev_entrypoint.html" if app.debug else "prod_entrypoint.html",
         static_build_path=static_build_path,
+        user_id=user.id if user else None,
     )
+
 
 @plugin_views.route("/setup", methods=["GET", "POST"])
 def setup():
@@ -28,16 +33,12 @@ def setup():
     if not session.get("nonce"):
         session["nonce"] = generate_nonce()
     if request.method == "POST":
-
         # Administration
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
 
-
-        admin = Admins(
-            name=name, email=email, password=password, type="admin", hidden=True
-        )
+        admin = Admins(name=name, email=email, password=password, type="admin", hidden=True)
 
         try:
             db.session.add(admin)
@@ -49,5 +50,3 @@ def setup():
         "dev_entrypoint.html" if app.debug else "prod_entrypoint.html",
         static_build_path=static_build_path,
     )
-
-
