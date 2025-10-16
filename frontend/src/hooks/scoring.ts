a4dcf79179bdde00dda8bfe8fd29f6b46c48501a
Daniel Rogers <drogers@cyberskyline.com>
2025-10-16 13:01:45 -0700

    scoring

--- frontend/src/hooks/scoring.ts
+++ frontend/src/hooks/scoring.ts
@@ -10,40 +10,40 @@ import useSWR, { mutate } from 'swr';
 
 export function useMyTeamScore(eventId: number | null) {
   return useSWR<Score, Error>(
-    eventId ? `/events/${eventId}/me/team/score` : null,
+    eventId ? `/scoring/${eventId}/me/team/score` : null,
   );
 }
 
 /* ADMIN ENDPOINTS */
 export function useTeamScoreHistory(eventId: number| null, teamId: number | null) {
   return useSWR<{score_events: ScoreEvent[]}, Error>(
-    (eventId && teamId) ? `/admin/scoring/events/${eventId}/teams/${teamId}/history` : null,
+    (eventId && teamId) ? `/admin/scoring/teams/${teamId}/history` : null,
   );
 }
 
 export function useTeamAttempts(eventId: number | null, teamId: number | null) {
   return useSWR<Attempt[], Error>(
-    (eventId && teamId) ? `/admin/scoring/events/${eventId}/teams/${teamId}/attempts` : null,
+    (eventId && teamId) ? `/admin/scoring/teams/${teamId}/attempts` : null,
   );
 }
 
 export function useTeamHintRedemptions(eventId: number | null, teamId: number | null) {
   return useSWR<HintRedemption[], Error>(
-    (eventId && teamId) ? `/admin/scoring/events/${eventId}/teams/${teamId}/hint_redemptions` : null,
+    (eventId && teamId) ? `/admin/scoring/teams/${teamId}/hint_redemptions` : null,
   );
 }
 
 export function useTeamManualAwards(eventId: number | null, teamId: number | null) {
   return useSWR<ManualPointAward[], Error>(
-    (eventId && teamId) ? `/admin/scoring/events/${eventId}/teams/${teamId}/manual_awards` : null,
+    (eventId && teamId) ? `/admin/scoring/teams/${teamId}/manual_awards` : null,
   );
 }
 
-export function adjustPoints(eventId: number, teamId: number, points: number, reason: string) {
-  return apiMutation(`/admin/scoring/events/${eventId}/teams/${teamId}/award-points`, { points, reason }, {
+export function adjustPoints(teamId: number, points: number, reason: string) {
+  return apiMutation(`/admin/scoring/teams/${teamId}/award-points`, { points, reason }, {
     method : 'POST',
   }).then(() => {
-    mutate(`/admin/scoring/events/${eventId}/teams/${teamId}/history`);
-    mutate(`/admin/scoring/events/${eventId}/teams/${teamId}/manual_awards`);
+    mutate(`/admin/scoring/teams/${teamId}/history`);
+    mutate(`/admin/scoring/teams/${teamId}/manual_awards`);
   });
 }
