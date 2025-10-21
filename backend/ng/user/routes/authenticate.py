19f7811102d95a2b6296bfb49e929880c3e53154
Zoe Coughlan <zcoughlan@cyberskyline.com>
2025-10-21 13:21:10 -0400

    added decorator to oauth routes and set up swarm for prodmode (#254)

--- backend/ng/user/routes/authenticate.py
+++ backend/ng/user/routes/authenticate.py
@@ -1,15 +1,20 @@
 # backend/ng/user/routes.py
 from flask_restx import Namespace, Resource
 from ..controllers.authenticate import okta_login, okta_callback
+from ...core.middleware import (
+    public_endpoint,
+)
 
 oauth_namespace = Namespace("authenticate", description="Okta OAuth routes")
 
 @oauth_namespace.route("/okta/login")
 class OktaLogin(Resource):
+    @public_endpoint()
     def get(self):
         return okta_login()
 
 @oauth_namespace.route("/okta/callback")
 class OktaCallback(Resource):
+    @public_endpoint()
     def get(self):
         return okta_callback()
