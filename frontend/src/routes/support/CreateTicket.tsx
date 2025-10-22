13e69333ab08be2fa882dbd4fcabf3374eee99ec
Caitlin Jones <cjones@cyberskyline.com>
2025-10-22 17:59:51 +0000

    Update create ticket form

--- frontend/src/routes/support/CreateTicket.tsx
+++ frontend/src/routes/support/CreateTicket.tsx
@@ -5,50 +5,97 @@ import {
   Flex,
   Heading,
   TextField,
-  Select,
 } from '@radix-ui/themes';
 import { TbArrowLeft } from 'react-icons/tb';
 import { useNavigate } from 'react-router';
-import RichTextEditor from 'components/RichTextEditor';
 import { useState } from 'react';
-import { Form } from 'radix-ui';
-import { isEmpty, isUndefined, map } from 'lodash';
+import { isUndefined, map } from 'lodash';
+import {
+  Controller,
+  useForm,
+  type FieldError,
+  type Control,
+} from 'react-hook-form';
+import RichTextEditor from 'components/RichTextEditor';
+import FormField from 'components/FormField';
+import SelectDropdown from 'components/SelectDropdown';
+import RequireEventPermission from 'components/RequireEventPermission';
+import { ErrorCallout } from 'components/Callouts';
 import { createTicket } from '@/hooks/support';
 import { useMyEvents } from '@/hooks/events';
 import { useMyChallenges } from '@/hooks/challenge';
-import { ErrorCallout } from 'components/Callouts';
+
+type CreateInputs = {
+  subject: string,
+  text: string,
+  event_id?: string,
+  challenge_id?: string,
+}
+
+function ChallengesDropdown({ control, error, eventId }: {control: Control<CreateInputs>, error?: FieldError, eventId: number}) {
+  const { data : challenges, error : challengeError } = useMyChallenges(eventId);
+  const challengeOptions: {value: string, name: string}[] = map(
+    challenges,
+    (chall) => ({ value : chall.challenge_id.toString(), name : chall.challenge_name }),
+  );
+
+  if (!isUndefined(challengeError)) {
+    return <ErrorCallout>{challengeError?.message}</ErrorCallout>;
+  }
+  return (
+    <SelectDropdown
+      control={control}
+      rules={{
+        validate : (value) => value !== '' || 'Please select a challenge or None',
+      }}
+      error={error}
+      name="challenge_id"
+      label="Challenge"
+      options={challengeOptions}
+      noneOption
+    />
+  );
+}
 
 export default function CreateTicket() {
   const navigate = useNavigate();
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState : { errors },
+    watch,
+  } = useForm<CreateInputs>({
+    defaultValues : {
+      subject : undefined,
+      text : undefined,
+      event_id : '',
+      challenge_id : '',
+    },
+  });
+
+  const watchedEvent: string | undefined = watch('event_id');
+
   const [ error, setError ] = useState<string | null>(null);
   const [ loading, setLoading ] = useState<boolean>(false);
-  const [ text, setText ] = useState<string>();
-  const [ selectedEvent, setSelectedEvent ] = useState<string | undefined>();
-  const [ selectedChallenge, setSelectedChallenge ] = useState<string | undefined>();
 
   const { data : events, error : eventsError } = useMyEvents();
-  const { data : challenges, error : challengeError } = useMyChallenges(Number(selectedEvent));
+  const eventOptions: {value: string, name: string}[] = map(events, ({ id, name }) => ({ value : id.toString(), name }));
 
   if (!isUndefined(eventsError)) {
     return <ErrorCallout>{eventsError?.message}</ErrorCallout>;
-  } if (!isUndefined(challengeError)) {
-    <ErrorCallout>{challengeError?.message}</ErrorCallout>;
   }
 
-  const create = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const create = async (data: CreateInputs) => {
     setLoading(true);
 
-    const formData = new FormData(e.currentTarget);
-    const data = Object.fromEntries(formData.entries());
-    const parsed = {
-      subject : data?.subject as string,
-      text : data?.text as string,
-      event_id : data?.event_id ? Number(data.event_id) : undefined,
-      challenge_id : data?.challenge_id ? Number(data.challenge_id) : undefined,
-    };
-
-    createTicket(parsed).then((ticketId) => {
+    createTicket({
+      subject : data.subject,
+      text : data.text,
+      event_id : Number(data.event_id) || undefined,
+      challenge_id : Number(data.challenge_id) || undefined,
+    }).then((ticketId) => {
       navigate(`/support/${ticketId}`);
     }).catch((err) => {
       setError(err.message);
@@ -72,105 +119,72 @@ export default function CreateTicket() {
         </Box>
         <Heading size="7">Create a New Support Ticket</Heading>
 
-        <Form.Root onSubmit={create}>
-          <Form.Field name="subject">
-            <Form.Label>Subject: *</Form.Label>
-            <Form.Control asChild>
+        <form
+          onSubmit={handleSubmit(create)}
+        >
+          <FormField label="Subject" error={errors?.subject}>
+            {(injected) => (
               <TextField.Root
-                placeholder="Enter a subject for your ticket"
-                required
+                placeholder="Subject"
+                {...register('subject', { required : 'Subject is required' })}
+                {...injected}
               />
-            </Form.Control>
-            <Form.Message match="valueMissing">
-              Subject is a required field
-            </Form.Message>
-          </Form.Field>
-
-          <Form.Field name="text">
-            <Form.Label>Message: *</Form.Label>
-            {/* Hidden input to track value and error handling */}
-            <Form.Control asChild>
-              <input
-                type="hidden"
-                name="text"
-                value={text}
-                required
-              />
-            </Form.Control>
-            {/* Displayed editor */}
-            <RichTextEditor
-              initialValue={text}
-              onChange={setText}
-            />
-            {text === '' && (
-              <Form.Message>
-                Message is a required field.
-              </Form.Message>
             )}
-          </Form.Field>
+          </FormField>
+          <FormField label="Message" error={errors?.text}>
+            {() => (
+              <Controller
+                name="text"
+                control={control}
+                rules={{ required : 'Message is required' }}
+                render={({ field }) => (
+                  <RichTextEditor
+                    initialValue={field.value}
+                    onChange={field.onChange}
+                  />
+                )}
+              />
+            )}
+          </FormField>
 
-          <Form.Field name="event_id" className="mt-2">
-            <Form.Label className="mr-2">Event:</Form.Label>
-            <Select.Root
-              name="event_id"
-              value={selectedEvent}
-              onValueChange={(v: string) => setSelectedEvent(v)}
-            >
-              <Select.Trigger placeholder="Select an event" className="w-[200px]" />
-              <Select.Content>
-                <Select.Group>
-                  {map(events, (event) => (
-                    <Select.Item
-                      key={event.id}
-                      value={String(event.id)}
-                    >
-                      {event.name}
-                    </Select.Item>
-                  ))}
-                </Select.Group>
-              </Select.Content>
-            </Select.Root>
-          </Form.Field>
+          <SelectDropdown
+            control={control}
+            rules={{
+              validate : (value) => value !== '' || 'Please select an event or None',
+            }}
+            error={errors?.event_id}
+            name="event_id"
+            label="Event"
+            options={eventOptions}
+            noneOption
+          />
 
-          {!isEmpty(selectedEvent)
+          {watchedEvent !== 'None'
             && (
-              <Form.Field name="challenge_id" className="mt-2">
-                <Form.Label className="mr-2">Challenge:</Form.Label>
-                <Select.Root
-                  name="challenge_id"
-                  value={selectedChallenge}
-                  onValueChange={(v: string) => setSelectedChallenge(v)}
-                >
-                  <Select.Trigger placeholder="Select a challenge" />
-                  <Select.Content>
-                    <Select.Group>
-                      {map(challenges, (challenge) => (
-                        <Select.Item
-                          key={challenge.challenge_id}
-                          value={String(challenge.challenge_id)}
-                        >
-                          {challenge.challenge_name}
-                        </Select.Item>
-                      ))}
-                    </Select.Group>
-                  </Select.Content>
-                </Select.Root>
-              </Form.Field>
+              <RequireEventPermission
+                eventId={Number(watchedEvent)}
+                permission="CAN_VIEW_CHALLENGES"
+                permissionDeniedPlaceholder={null}
+              >
+                <ChallengesDropdown
+                  control={control}
+                  error={errors?.challenge_id}
+                  eventId={Number(watchedEvent)}
+                />
+              </RequireEventPermission>
             )}
 
           {error && <ErrorCallout className="mt-2">{error}</ErrorCallout>}
+          <Button
+            type="submit"
+            className="!mt-2"
+            loading={loading}
+            disabled={loading}
+          >
+            Submit Ticket
+          </Button>
+        </form>
 
-          <Form.Submit asChild>
-            <Button
-              type="submit"
-              className="!mt-2"
-              loading={loading}
-              disabled={loading}
-            >
-              Submit Ticket
-            </Button>
-          </Form.Submit>
-        </Form.Root>
       </Flex>
     </Container>
   );
