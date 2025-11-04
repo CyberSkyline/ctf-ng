2fbd570824cf82b4cf89590c5dac0de8f9e80512
Ubuntu <tmessner@cyberskyline.com>
2025-11-04 17:06:16 +0000

    Remove user ability to restart containers

--- backend/ng/containers/controllers/reboot_containers.py
+++
@@ -1,9 +0,0 @@
-from ..models.ContainerInstance import ContainerInstance
-
-def reboot_containers(challenge_id: int, team_id: int, current_user: int) -> bool:
-    instances = ContainerInstance.get_instance_group(challenge_id, team_id)
-
-    for instance in instances:
-        instance.restart()
-
-    return True
