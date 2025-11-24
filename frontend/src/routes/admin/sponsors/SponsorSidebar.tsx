f03d807a1179443d5f63bc5f403cb3a86eb988d8
Caitlin Jones <cjones@cyberskyline.com>
2025-11-24 19:35:29 +0000

    Sponsor Sidebar

--- frontend/src/routes/admin/sponsors/SponsorSidebar.tsx
+++ frontend/src/routes/admin/sponsors/SponsorSidebar.tsx
@@ -1,21 +1,35 @@
 import type { Sponsor } from '@/types';
 import AdminSidebar from 'components/AdminSidebar';
 import AdminSidebarHeader from 'components/AdminSidebarHeader';
-import { WarningCallout } from 'components/Callouts';
+import { ErrorCallout, WarningCallout } from 'components/Callouts';
 import SponsorModal from './SponsorModal';
+import { useFileUrl } from '@/hooks/fileuploads';
+import { Box, DataList } from '@radix-ui/themes'
 
 export default function SponsorSidebar({ entity }: {entity: Sponsor}) {
+  const { data, error } = useFileUrl('sponsor-logos', entity.logo);
+
   return (
     <AdminSidebar>
       <AdminSidebarHeader title={entity.name}>
         <SponsorModal sponsor={entity} />
       </AdminSidebarHeader>
+      <DataList.Root>
+        <DataList.Item>
+          <DataList.Label>Name</DataList.Label>
+          <DataList.Value>{entity.name}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label>Filename</DataList.Label>
+          <DataList.Value>{entity.logo}</DataList.Value>
+        </DataList.Item>
+      </DataList.Root>
       {entity.logo ? (
-        <>
-          <p>{entity.logo}</p>
-          <img src={entity.logo} alt={entity.name} />
-        </>
+        <Box maxHeight='500px'>
+          <img src={data?.url} alt={data?.filename} />
+        </Box>
       ) : <WarningCallout>No logo is associated with this sponsor</WarningCallout>}
+      {error && <ErrorCallout>{error.message}</ErrorCallout>}
     </AdminSidebar>
   );
 }
