46ee700c9af254406d43faf31a564697b3922403
Zoe Coughlan <zcoughlan@cyberskyline.com>
2025-11-24 14:31:47 -0500

    staging updates for static site (#425)
    * staging updates for static site
    
    * cleaned up TODOs and properly passed in BASE_PATH
    
    * removed console log for testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ command }) => ({
   define : {
     BUILD_MODE : command === 'build',
     RELEASE : JSON.stringify(RELEASE),
+    BASE_PATH : JSON.stringify(command === 'build' ? '/ctf' : '/'),
   },
   server : {
     allowedHosts : [ '.cisa.gov', '.localhost' ],
