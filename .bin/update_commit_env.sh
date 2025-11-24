d53ffb82329d1c61ef51a74fa8a15a8024e00325
Franz Payer <fpayer@cyberskyline.com>
2025-11-24 19:50:15 +0000

    Added script to automatically update the commit hash in the ENV var for prod

---
+++ .bin/update_commit_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR=$(dirname $BASH_SOURCE)
+source $DIR/utils.sh
+
+PROD_ENV_PATH=$(realpath "$DIR/../.env.prod")
+
+COMMIT_HASH=$(git rev-parse HEAD)
+STATIC_BUILD_PATH="/dist/$COMMIT_HASH"
+
+# Update the STATIC_BUILD_PATH in the .env file using sed
+sed -i "s|^STATIC_BUILD_PATH=.*|STATIC_BUILD_PATH=$STATIC_BUILD_PATH|" "$PROD_ENV_PATH"
+
+echo "Updated STATIC_BUILD_PATH to: $STATIC_BUILD_PATH"
