1d2376c7e1b4fbc7c378d69a79914d8660df8609
Ubuntu <tmessner@cyberskyline.com>
2025-12-01 16:09:52 +0000

    Web editor merge conflict mess up

--- backend/ng/challenge/models/ContainerBlueprint.py
+++ backend/ng/challenge/models/ContainerBlueprint.py
@@ -181,7 +181,7 @@ class ContainerBlueprint(db.Model):
         return {k: (v(team_seed=team_seed) if isinstance(v, EnvVarRenderer) else v) for k, v in self.environment.items()}
 
 
-    def pull_image(self):
+    def pull_image(self, user_id):
         DOCKER_HOST = get_app_config("DOCKER_HOST")
 
         client = get_client(DOCKER_HOST)
