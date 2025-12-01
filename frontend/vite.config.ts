7b4d111b2230670ec65558976fdd5bcf37215d0b
trevnels-cs <207569523+trevnels-cs@users.noreply.github.com>
2025-12-01 17:08:53 -0500

    Use statically-served novnc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { spawnSync } from 'child_process';
 import path from 'path';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 function gitStatus(...args: string[]) {
   return spawnSync('git', args, { cwd : __dirname }).stdout.toString().trim();
@@ -19,7 +18,7 @@ export default defineConfig(({ command }) => ({
     BUILD_MODE : command === 'build',
     RELEASE : JSON.stringify(RELEASE),
     BASE_PATH : JSON.stringify(command === 'build' ? '/ctf' : ''),
-    PUBLIC_BASE : command === 'build' ? '/dist/public/' : '/static/public/',
+    PUBLIC_BASE : JSON.stringify(command === 'build' ? '/dist' : '/static'),
   },
   server : {
     allowedHosts : [ '.cisa.gov', '.localhost' ],
@@ -47,14 +46,6 @@ export default defineConfig(({ command }) => ({
         return undefined;
       },
     },
-    viteStaticCopy({
-      targets : [
-        {
-          src : path.resolve(__dirname, './public/'),
-          dest : './',
-        },
-      ],
-    }),
   ],
   resolve : {
     alias : {
@@ -77,5 +68,5 @@ export default defineConfig(({ command }) => ({
   optimizeDeps : {
     include : [ 'react', 'react-dom', 'react-router-dom' ],
   },
-  publicDir : command === 'serve' ? 'public' : false,
+  publicDir : 'public',
 }));
