16a4c4c482fc625348923a6d13a21dd88693ab0f
Ubuntu <tmessner@cyberskyline.com>
2025-12-02 14:31:55 +0000

    Move to a single competitor net

--- backend/ng/containers/controllers/start_containers.py
+++ backend/ng/containers/controllers/start_containers.py
@@ -3,6 +3,8 @@ from ...user.models.User import User
 from ..models.ContainerInstance import ContainerInstance
 from ..models.IndvidualContainer import IndvidualContainer
 from ...challenge.models.ContainerBlueprint import ContainerBlueprint
+from ..constants import CHALLENGER_NET_NAME
+from ...core import BusinessLogicError
 
 def start_containers(challenge_id: int, team_id: int, current_user: User) -> bool:
     blueprints = ContainerBlueprint.query.filter_by(challenge_id=challenge_id).all()
@@ -18,7 +20,8 @@ def start_containers(challenge_id: int, team_id: int, current_user: User) -> boo
     indvidual_ctr = IndvidualContainer.create_indvidual_container(current_user.id)
     indvidual_ctr.disconnect_from_networks()
 
-    for network in set(networks):
-        indvidual_ctr.connect_to_network(ContainerInstance.render_network_name(team_id, network, challenge_id))
+    if CHALLENGER_NET_NAME not in set(networks):
+        raise BusinessLogicError("Challenge has no challenger network")
 
+    indvidual_ctr.connect_to_network(ContainerInstance.render_network_name(team_id, CHALLENGER_NET_NAME, challenge_id))
     return True
