1d0f1ed282c93a4ecef938a9593d9418f29ffb81
trevnels-cs <207569523+trevnels-cs@users.noreply.github.com>
2025-12-03 12:40:07 -0500

    Fix broken merge

--- frontend/src/hooks/users.ts
+++ frontend/src/hooks/users.ts
@@ -197,21 +197,3 @@ export function unbanUser(userId: number) {
     mutate('/admin/users');
   });
 }
-
-export function banUser(userId: number) {
-  return apiMutation(`/admin/users/${userId}/ban`, {}, {
-    method : 'POST',
-  }).then(() => {
-    mutate(`/admin/users/${userId}`);
-    mutate('/admin/users');
-  });
-}
-
-export function unbanUser(userId: number) {
-  return apiMutation(`/admin/users/${userId}/unban`, {}, {
-    method : 'POST',
-  }).then(() => {
-    mutate(`/admin/users/${userId}`);
-    mutate('/admin/users');
-  });
-}
