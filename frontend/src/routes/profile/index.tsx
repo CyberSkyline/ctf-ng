d4b06a8f101c6bf001e38eafe5ca180ed701ef12
Caitlin Jones <cjones@cyberskyline.com>
2025-12-04 19:06:44 +0000

    Constrain displayed images

--- frontend/src/routes/profile/index.tsx
+++ frontend/src/routes/profile/index.tsx
@@ -77,7 +77,7 @@ export default function Profile() {
             <>
               <p>{mySponsor.name}</p>
               {image?.download_url && (
-                <Box maxHeight="500px">
+                <Box maxHeight="500px" maxWidth="500px">
                   <img src={image?.download_url} alt="" />
                 </Box>
               )}
