cf781306341d411bbf2a5548b61f7511049adf76
droge91 <142940847+droge91@users.noreply.github.com>
2025-12-05 12:57:31 -0800

    add route for get team info from code (#502)
    (MINOR)

--- backend/ng/event/tests/test_event_api.py
+++ backend/ng/event/tests/test_event_api.py
@@ -773,6 +773,40 @@ class Test_Event_Team_Lookup:
         data = response.get_json()
         assert data["success"] is False
 
+    def test_get_team_by_invite_code(
+        self,
+        logged_in_client,
+        user,
+        event_factory,
+        team_factory
+    ):
+        event = event_factory(name = "Event for Team Lookup by Invite Code", public = True)
+        team = team_factory(event = event, members = [user])
+
+        response = logged_in_client.get(
+            f"/ng/events/{event.id}/team/{team.invite_code}",
+        )
+
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data["success"] is True
+        assert data["data"] == team.serialize()
+
+    def test_get_team_by_invalid_invite_code(
+        self,
+        logged_in_client,
+        event_factory
+    ):
+        event = event_factory(name = "Event for Invalid Invite Code Lookup", public = True)
+
+        response = logged_in_client.get(
+            f"/ng/events/{event.id}/team/INVALIDCODE",
+        )
+
+        assert response.status_code == 404
+        data = response.get_json()
+        assert data["success"] is False
+
 
 class Test_Event_TeamMember_Lookup:
     def get_endpoint(self, event_id: int) -> str:
