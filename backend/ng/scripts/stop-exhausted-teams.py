3bf245549ef4c85f7a8b5c9349f4eb16da29b437
Ubuntu <tmessner@cyberskyline.com>
2025-12-16 14:23:04 +0000

    tmp

---
+++ backend/ng/scripts/stop-exhausted-teams.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from CTFd import create_app, CTFdFlask
+from CTFd.plugins.ng.team.models.Team import Team
+from CTFd.plugins.ng.challenge.models.Challenge import Challenge
+#from CTFd.plugins.ng.container.models.ContainerInstance import ContainerInstance
+from CTFd.plugins.ng.core.utils import utc_now
+from datetime import timedelta
+
+app: CTFdFlask = create_app()
+
+
+now = utc_now()
+delta = timedelta(days=1)
+query_end_date_start = now - delta
+ 
+with app.app_context():
+    res = Team.query.filter(Team.end_time >= query_end_date_start).all()
+    for team in res:
+        challenges = Challenge.query.filter(Challenge.event == team.event).all()
+        for challenge in challenges:
+            print(challenge.id, team.id)
+            #ContainerInstance.stop(challenge.id, team.id)
