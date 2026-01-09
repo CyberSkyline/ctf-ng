73c78e9696a8349ac4d278e0ae147f6429b3db3b
johnmarsden-edu <research@johnmarsden.dev>
2026-01-09 12:41:35 -0500

    Load test improvements (#629)
    * Ignore tls errors and make duration variable
    
    * Cleanup the scripts
    
    * Maybe event ids are strings?
    
    * Live Updates During Load Test
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@cyberskyline.com>

--- tools/load-test/index.ts
+++ tools/load-test/index.ts
@@ -6,37 +6,56 @@ import { make_user, User } from './model/user.ts';
 import { DefaultHttpUserScenario } from './scenarios/default_http_user.ts';
 
 const BASE_URL = __ENV.BASE_URL || 'http://localhost';
+const STAGE_ONE_DURATION = __ENV.STAGE_ONE_DURATION || '5s';
+const STAGE_TWO_DURATION = __ENV.STAGE_TWO_DURATION || '10s';
+const STAGE_THREE_DURATION = __ENV.STAGE_THREE_DURATION || '5s';
 const STAGE_ONE_VUS = Number(__ENV.STAGE_ONE_VUS) || 2;
 const STAGE_TWO_VUS = Number(__ENV.STAGE_TWO_VUS) || 5;
 const STAGE_THREE_VUS = Number(__ENV.STAGE_THREE_VUS) || 10;
 
+const eventId = JSON.parse(open('./data.json'))['event_id'];
+if (!eventId) {
+  throw new Error('event_id key missing in data.json');
+}
+
+const challengeId = JSON.parse(open('./data.json'))['challenge_id'];
+if (!challengeId) {
+  throw new Error('challenge_id key missing in data.json');
+}
+
+const questionId = JSON.parse(open('./data.json'))['question_id'];
+if (!questionId) {
+  throw new Error('question_id key missing in data.json');
+}
+
 const adminUsers: User[] = new SharedArray('adminUsers', function () {
-  const data = JSON.parse(open('./users.json'))['admins'];
+  const data = JSON.parse(open('./data.json'))['admins'];
   if (!Array.isArray(data)) {
-    throw new Error('admins key in users.json must be an array');
+    throw new Error('admins key in data.json must be an array');
   }
 
   return data.map((u: any, i: number) => make_user(i, u.email, u.password, BASE_URL, 'admin'));
 });
 
 const defaultUsers: User[] = new SharedArray('defaultUsers', function () {
-  const data = JSON.parse(open('./users.json'))['users'];
+  const data = JSON.parse(open('./data.json'))['users'];
   if (!Array.isArray(data)) {
-    throw new Error('users key in users.json must be an array');
+    throw new Error('users key in data.json must be an array');
   }
 
   return data.map((u: any, i: number) => make_user(i, u.email, u.password, BASE_URL));
 });
 
 export const options = {
+  insecureSkipTLSVerify: true,
   scenarios: {
     defaultHttpUser: {
       executor: 'ramping-vus',
       startVUs: 1,
       stages: [
-        { duration: '5s', target: STAGE_ONE_VUS }, // ramp to STAGE_ONE_VUS
-        { duration: '10s', target: STAGE_TWO_VUS },  // ramp to STAGE_TWO_VUS
-        { duration: '5s', target: STAGE_THREE_VUS },   // ramp to STAGE_THREE_VUS
+        { duration: STAGE_ONE_DURATION, target: STAGE_ONE_VUS }, // ramp to STAGE_ONE_VUS
+        { duration: STAGE_TWO_DURATION, target: STAGE_TWO_VUS },  // ramp to STAGE_TWO_VUS
+        { duration: STAGE_THREE_DURATION, target: STAGE_THREE_VUS },   // ramp to STAGE_THREE_VUS
       ],
       gracefulRampDown: '30s',
       exec: 'defaultHttpUser',
@@ -106,7 +125,7 @@ function getDefaultUser() {
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
 export async function defaultHttpUser() {
   let user = getDefaultUser();
-  let scenario = new DefaultHttpUserScenario(user);
+  let scenario = new DefaultHttpUserScenario(user, eventId, challengeId, questionId);
   scenario.execute();
   sleep(1);
 }
