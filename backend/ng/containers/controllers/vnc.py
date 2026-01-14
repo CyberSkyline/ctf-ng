d103527d07bf7dd66c1dca4beddf95187511cf4e
Ubuntu <tmessner@cyberskyline.com>
2026-01-14 18:37:30 +0000

    Change cache control to explicitly not cache

--- backend/ng/containers/controllers/vnc.py
+++ backend/ng/containers/controllers/vnc.py
@@ -2,12 +2,14 @@ from ..models.IndvidualContainer import IndvidualContainer
 from flask import Response
 
 def forward_vnc(user_id: int) -> Response:
+    print(user_id)
     indv_ctr = IndvidualContainer.create_indvidual_container(user_id)
 
     host_port = indv_ctr.get_novnc_port()
+    print(host_port)
 
     headers = {
-       "Cache-Control": "max-age=60",
+       "Cache-Control": "no-store",
        "Host-Ip": indv_ctr.hostip,
        "Host-Port": host_port
     }
