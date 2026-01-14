195b356a445b10c2301f3aba9c5c2e06eb140fb7
Ubuntu <tmessner@cyberskyline.com>
2026-01-14 18:41:22 +0000

    Remove missed prints

--- backend/ng/containers/controllers/vnc.py
+++ backend/ng/containers/controllers/vnc.py
@@ -2,11 +2,9 @@ from ..models.IndvidualContainer import IndvidualContainer
 from flask import Response
 
 def forward_vnc(user_id: int) -> Response:
-    print(user_id)
     indv_ctr = IndvidualContainer.create_indvidual_container(user_id)
 
     host_port = indv_ctr.get_novnc_port()
-    print(host_port)
 
     headers = {
        "Cache-Control": "no-store",
