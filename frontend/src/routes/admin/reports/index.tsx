8065ea3e56ee656dc95493988772ca2acfab0042
trevnels-cs <207569523+trevnels-cs@users.noreply.github.com>
2026-01-16 12:30:26 -0500

    Set up embedded grafana for reports (#623)
    * Initial grafana setup
    
    * Move grafana to /admin/grafana, cache auth request for 1m
    
    * Only set nav to undocked initially
    
    * Set up grafana provisioning and test dashboard
    
    * Production config
    
    * Update reverse proxy config

---
+++ frontend/src/routes/admin/reports/index.tsx
@@ -0,0 +1,28 @@
+import { Card, Inset } from '@radix-ui/themes';
+import { useTheme } from 'next-themes';
+import { useEffect } from 'react';
+
+export default function AdminReports() {
+  useEffect(() => {
+    if (localStorage.getItem('grafana.navigation.docked') === null) {
+      localStorage.setItem('grafana.navigation.docked', 'false');
+    }
+  }, []);
+
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <>
+      <title>Admin Reports</title>
+      <Card className="h-full w-full !flex flex-col">
+        <Inset side="all" className="grow shrink">
+          <iframe
+            title="Grafana"
+            className="w-full h-full"
+            src={`/admin/grafana/dashboards?theme=${resolvedTheme}`}
+          />
+        </Inset>
+      </Card>
+    </>
+  );
+}
