b6621775e1f329517df2da4e14bd0cc4b312453c
Franz Payer <fpayer@cyberskyline.com>
2026-01-28 09:59:59 -0500

    Conf updates (#653)

--- conf/ctfd/entrypoint.sh
+++ conf/ctfd/entrypoint.sh
@@ -54,6 +54,7 @@ start_prod() {
   echo "Starting CTFd"
   exec gunicorn 'CTFd:create_app()' \
       --bind '0.0.0.0:8000' \
+      --timeout 120 \
       --workers $WORKERS \
       --worker-tmp-dir "$WORKER_TEMP_DIR" \
       --worker-class "$WORKER_CLASS" \
