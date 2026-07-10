2ddc44ebd490b59062cef8a7d66a288957608e51
Caitlin Jones <cjones@cyberskyline.com>
2026-07-10 21:11:32 +0000

    Accordion component on past dashboard events

--- frontend/src/routes/dashboard/PastEvents.tsx
+++ frontend/src/routes/dashboard/PastEvents.tsx
@@ -1,11 +1,21 @@
 import { useMyEvents } from '@/hooks/events';
-import { Heading } from '@radix-ui/themes';
+import { Grid, Heading } from '@radix-ui/themes';
 import { ErrorCallout } from 'components/Callouts';
-import EventGrid from 'components/EventGrid';
+import Accordion from 'components/Accordion';
+import { useMemo } from 'react';
+import { chain } from 'lodash';
+import EventCard from './EventCard';
 
 export default function PastEvents() {
   const { data, error } = useMyEvents();
   const pastEvents = data?.filter((event) => event.end_time && new Date() > event.end_time);
+
+  const groupedEvents = useMemo(() => chain(pastEvents)
+    .sortBy((event) => event.start_time?.getTime() || 0)
+    .groupBy((event) => event.start_time?.getFullYear()?.toString() || 'Unknown')
+    .toPairs()
+    .reverse()
+    .value(), [ pastEvents ]);
 
   if (error) {
     return (
@@ -24,7 +34,34 @@ export default function PastEvents() {
   return (
     <>
       <Heading size="6">Your Past Events</Heading>
-      <EventGrid events={pastEvents || []} group />
+
+      <Accordion.Root type="multiple" defaultValue={groupedEvents.length ? [ groupedEvents[0][0] ] : []}>
+        {groupedEvents.map(([ year, eventsInYear ]) => (
+          <Accordion.Item key={year} value={year}>
+            <Accordion.Header>
+              <Accordion.Trigger>
+                {year}
+              </Accordion.Trigger>
+            </Accordion.Header>
+
+            <Accordion.Content>
+              <Grid
+                columns={{
+                  initial : '1', xs : '1', sm : '2', lg : '3',
+                }}
+                gap="3"
+              >
+                {eventsInYear.map((event) => (
+                  <EventCard
+                    key={event.id}
+                    event={event}
+                  />
+                ))}
+              </Grid>
+            </Accordion.Content>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
     </>
   );
 }
