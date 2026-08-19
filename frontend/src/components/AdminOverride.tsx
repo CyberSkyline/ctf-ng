54b96139ed30fae7d1879c7be41eb58546b6c490
cjones-cs <cjones@cyberskyline.com>
2026-08-19 09:52:04 -0400

    Feature/admin override badge (#702)
    * Admin Override generic fetcher
    
    * generic component for Admin Override badge or callout
    
    * Add admin override logic to event leaderboard
    
    * Add default text
    
    * Fix type and lint errors

---
+++ frontend/src/components/AdminOverride.tsx
@@ -0,0 +1,46 @@
+import { COLOR_WARNING } from '@/constants';
+import { Box, Badge, Callout } from '@radix-ui/themes';
+import type { ComponentProps } from 'react';
+import { TbAlertTriangle } from 'react-icons/tb';
+
+type Props =
+  | ({ type: 'callout' } & ComponentProps<typeof Callout.Root>)
+  | ({ type: 'badge' } & ComponentProps<typeof Badge>);
+
+export default function AdminOverride(props: Props) {
+  // eslint-disable-next-line react/destructuring-assignment
+  if (props.type === 'callout') {
+    const { children, ...calloutProps } = props;
+
+    return (
+      <Callout.Root
+        variant="surface"
+        color={COLOR_WARNING}
+        {...calloutProps}
+      >
+        <Callout.Icon>
+          <TbAlertTriangle aria-label="Warning" />
+        </Callout.Icon>
+
+        <Callout.Text className="whitespace-pre-wrap">
+          {children || 'ADMIN VIEW'}
+        </Callout.Text>
+      </Callout.Root>
+    );
+  }
+
+  const { children, ...badgeProps } = props;
+
+  return (
+    <Box>
+      <Badge
+        color={COLOR_WARNING}
+        size="3"
+        radius="full"
+        {...badgeProps}
+      >
+        {children || 'ADMIN VIEW'}
+      </Badge>
+    </Box>
+  );
+}
